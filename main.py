af2f1de109dc843df483e6415b09cbf39503fdca
Hugo <81880255+JulyCrab@users.noreply.github.com>
2022-03-18 16:44:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #This code is to import tkinter libraries remeber to do py install tk!
-#This code is under the license of 
+#This code is under the license of Creative Commons Zero v1.0 Universal
 from tkinter import *
 from tkinter.ttk import *
 
@@ -12,7 +12,7 @@ def time():
     string = strftime("%H:%M:%S %p")
     label.config(text = string)
     label.after(1000, time)
-
+#To make the clock format 12 hrs. Simply change the strftime('%H:%M:%S %p') to strftime('%I:%M:%S %p').    
 label = Label(root, font = "ds-digital 100", background = "white", foreground = "black")
 label.pack(anchor = "center")
 
